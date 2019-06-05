87c02510461013d59074333583b5d72b1e9e64a7
Laptop <Laptop@10.126.254.45>
2019-06-05 21:46:23 +0200

    Anmeldetests hinzugefügt

--- src/test/java/sumz_selenium/sumz_selenium/AnmeldenTest.java
+++ src/test/java/sumz_selenium/sumz_selenium/AnmeldenTest.java
@@ -2,30 +2,170 @@ package sumz_selenium.sumz_selenium;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.Date;
 
-public class AnmeldenTest extends BaseTest{
-  
-  @Test
-  public void anmelden(){
-	    driver.get("http://sumz1718.dh-karlsruhe.de/login");
-	    driver.manage().window().setSize(new Dimension(1500, 849));
-	    driver.findElement(By.id("email")).click();
-	    driver.findElement(By.id("email")).sendKeys("olistraub@web.de");
-	    driver.findElement(By.id("password")).click();
-	    driver.findElement(By.id("password")).sendKeys("12345678aA#");
-	    driver.findElement(By.cssSelector(".login-btn")).click();
-	    
-	    new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/"));
-	    
-	    Assert.assertTrue(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/"));
-  }
-  
-  @Test
-  public void registrieren() {
-	 
-  }
+public class AnmeldenTest extends BaseTest {
+
+	@Test
+	public void anmelden_richtig() {
+
+		anmeldung("olistraub@web.de", "12345678aA#");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertTrue(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/"));
+	}
+
+	@Test
+	public void anmelden_falsche_mail() {
+
+		anmeldung("diesemailgibtesnicht@sicher.de", "12345678aA#");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/"));
+	}
+	
+	@Test
+	public void anmelden_falsches_passwort() {
+
+		anmeldung("olistraub@web.de", "falschesPasswort");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/"));
+	}
+
+	@Test
+	public void registrieren_richtig() {
+
+		Date date = new Date();
+
+		long time = date.getTime();
+		
+		registrierung(time + "@hewfuhiweuhf.de", "Qwer1234#", "Qwer1234#");
+		
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertTrue(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void registrieren_besthende_mail() {
+		
+		registrierung("olistraub@web.de", "Qwer1234#", "Qwer1234#");
+		
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void registrieren_schwaches_passwort() {
+		
+		Date date = new Date();
+
+		long time = date.getTime();
+		
+		registrierung(time + "@hewfuhiweuhf.de", "12345678", "12345678");
+		
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void registrieren_nicht_uebereinstimmende_passwoerter() {
+		
+		Date date = new Date();
+
+		long time = date.getTime();
+		
+		registrierung(time + "@hewfuhiweuhf.de", "Qwer1234#", "anderesPasswort");
+		
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void passwort_vergessen_richtig() {
+
+	    passwort_vergessen("eui22791@cndps.com");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertTrue(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void passwort_vergessen_nicht_existierende_mail() {
+
+	    passwort_vergessen("diesemailgibtesnicht@sicher.de");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	@Test
+	public void passwort_vergessen_falsches_mailformat() {
+
+	    passwort_vergessen("eui22791ATcndps.com");
+
+		try {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("http://sumz1718.dh-karlsruhe.de/login"));
+		} catch (Exception e) {
+
+		}
+
+		Assert.assertFalse(driver.getCurrentUrl().equals("http://sumz1718.dh-karlsruhe.de/login"));
+
+	}
+	
+	
 }
--- src/test/java/sumz_selenium/sumz_selenium/BaseTest.java
+++ src/test/java/sumz_selenium/sumz_selenium/BaseTest.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,6 +31,46 @@ public class BaseTest {
 	  @After
 	  public void tearDown() {
 	    driver.quit();
+	  }
+	  
+	  public void anmeldung(String mail, String passwort) {
+		  
+			driver.get("http://sumz1718.dh-karlsruhe.de/login");
+			driver.manage().window().setSize(new Dimension(1500, 849));
+			driver.findElement(By.id("email")).click();
+			driver.findElement(By.id("email")).sendKeys(mail);
+			driver.findElement(By.id("password")).click();
+			driver.findElement(By.id("password")).sendKeys(passwort);
+			driver.findElement(By.cssSelector(".login-btn")).click();
+		  
+		  
+	  }
+	  
+	  public void registrierung(String mail, String passwort1, String passwort2) {
+		  
+			driver.get("http://sumz1718.dh-karlsruhe.de/login");
+			driver.manage().window().setSize(new Dimension(945, 1020));
+			driver.findElement(By.cssSelector(".signup-btn > .mat-button-wrapper")).click();
+			driver.findElement(By.id("email")).click();
+			driver.findElement(By.id("email")).sendKeys(mail);
+			driver.findElement(By.id("password")).click();
+			driver.findElement(By.id("password")).sendKeys(passwort1);
+			driver.findElement(By.id("passwordrepeat")).click();
+			driver.findElement(By.id("passwordrepeat")).sendKeys(passwort2);
+			driver.findElement(By.cssSelector(".register-btn > .mat-button-wrapper")).click();
+		  
+		  
+	  }
+	  
+	  public void passwort_vergessen(String mail) {
+			  
+		    driver.get("http://sumz1718.dh-karlsruhe.de/login?returnUrl=%2F");
+		    driver.manage().window().setSize(new Dimension(945, 1020));
+		    driver.findElement(By.cssSelector(".forgot-btn > .mat-button-wrapper")).click();
+		    driver.findElement(By.id("email")).click();
+		    driver.findElement(By.id("email")).sendKeys(mail);
+		    driver.findElement(By.id("new-btn")).click();
+			  
 	  }
 	
 }

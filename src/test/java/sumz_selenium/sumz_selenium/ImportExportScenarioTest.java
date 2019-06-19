324fdfd9f56c0f4f1bd58a47c7bc10d2bc67de96
OL_SR <ol_sr@ec4u.com>
2019-06-19 14:00:20 +0200

    update/add import/export test

--- src/test/java/sumz_selenium/sumz_selenium/ImportExportScenarioTest.java
+++ src/test/java/sumz_selenium/sumz_selenium/ImportExportScenarioTest.java
@@ -15,14 +15,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class ImportExportScenarioTest extends BaseTest {
 
 	@Test
+	public void export_scenario_scenarioview() throws InterruptedException {
+		
+		anmeldung();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		WebElement scenario = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//mat-card[@id='455']")));
+		scenario.click();
+		
+		WebElement export = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*/button/span[text()=' Szenario exportieren ']")));
+		export.click();
+		
+		WebElement download = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//app-export-scenario/mat-nav-list/a")));
+		download.click();
+		
+		Thread.sleep(2000);
+		
+		String downloadLocation = System.getProperty("user.home")+ "\\Downloads";	
+		File file = new File(downloadLocation + "\\scenario.json");
+		Assert.assertTrue(file.exists());
+		file.delete();
+	}
+	
+	
+	@Test
 	public void export_scenario() throws InterruptedException {
 		
 		anmeldung();
-		
-		driver.findElement(By.xpath("//mat-card[@id='455']/mat-card-title/button")).click();
-		
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement export = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cdk-focused")));
+		
+		WebElement menu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//mat-card[@id='455']/mat-card-title/button")));
+		menu.click();
+	
+		WebElement export = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*/div[contains(@class, 'mat-menu-content')]/button[2]")));
 		export.click();
 		WebElement download = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//app-export-scenario/mat-nav-list/a")));
 		download.click();
@@ -34,7 +59,7 @@ public class ImportExportScenarioTest extends BaseTest {
 		file.delete();
 		
 	}
-	
+
 	@Test
 	public void import_scenario_valid() throws InterruptedException {
 		anmeldung();
@@ -46,16 +71,17 @@ public class ImportExportScenarioTest extends BaseTest {
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("select-button")));
 		
 		driver.findElement(By.xpath("//app-import-scenario/div[1]/input")).sendKeys(System.getProperty("user.dir") +"\\importFiles\\validScenario.json");
-		
+	
 		WebElement importButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("importScenario")));
 		importButton.click();
 		
 		WebElement scenario = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[mat-card-title/text()=' ImportScenarioTest ']")));
 		System.out.println(scenario.getAttribute("id"));
 			
-		driver.findElement(By.xpath("//mat-card[@id='"+scenario.getAttribute("id")+"']/mat-card-title/button")).click();
-		
-		WebElement delete = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".mat-menu-item:nth-child(2)")));
+		WebElement menu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//mat-card[@id='"+scenario.getAttribute("id")+"']/mat-card-title/button")));
+		menu.click();
+	
+		WebElement delete = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*/div[contains(@class, 'mat-menu-content')]/button[3]")));
 		delete.click();
 		
 		WebElement confirm = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".mat-warn > .mat-button-wrapper")));
